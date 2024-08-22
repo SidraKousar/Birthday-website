96b6e2fc6433208921babfc19864c8ab03a5cb10
SidraKousar <sidranaseem1290@gmail.com>
2024-08-22 15:35:29 +0500

    updated messages from team

--- src/components/EmployeeMessages.js
+++ src/components/EmployeeMessages.js
@@ -4,10 +4,10 @@ import { Box, Text, Grid } from '@chakra-ui/react';
 const employeeMessages = [
   { name: "Gul Muhammad", message: "Happy Birthday, Alexander! Your leadership and vision inspire us daily. Wishing you a year filled with success and happiness. Enjoy your special day!" },
   { name: "Tanveer Abbas", message: "Happy Birthday! 🎉 Even in my short time here, your transparent, flexible, and supportive leadership has made a strong impression. Wishing you a great day and a successful year ahead. 💼" },
-  { name: "Hassaan Ahmad", message: "Happy Birthday! Thanks for being an awesome boss!" },
-  { name: "Sufyan Arshad", message: "May your day be filled with joy and laughter. Happy Birthday!" },
+  { name: "Hassaan Ahmad", message: "Hey Alexander Wishing you a very Happy Birthday! 🎉On your special day, I want to express my gratitude for your leadership and guidance. Working under your direction has been an incredible experience, and I truly appreciate the wisdom and support you bring to our team.May this year bring you even more success, happiness, and great moments. Here's to another year of achieving amazing things!Enjoy your day to the fullest!" },
+  { name: "Sufyan Arshad", message: "Happy birthday @ Alexander. Wishing you a very happy birthday. Stay blessed always ❤" },
   { name: "Fatima Shabbir", message: "Wishing you a very Happy Birthday 🎉 Even though I have only been here for a few days, I have already seen how supportive, kind, and generous you are. I feel fortunate to be part of your team and look forward to learning and growing under your leadership. ✨" },
-  { name: "Aroosa Adil", message: "Happy Birthday! Grateful for your guidance and support." },
+  { name: "Aroosa Adil", message: "Hey @alexander. Happy birthday 🎉🧁! I've written your birthdate (22/08/2001) so many times while applying for you that it feels like it's my birthday today! haha jk 😅 But in all seriousness, I want to express my gratitude for the amazing leader and mentor you are and I'm looking forward to continuing to learn from you and achieving great things together. ✨🎉Wishing you an amazing day and a wonderful year ahead :p" },
   { name: "Sidra Naseem", message: "Happy Birthday! Wishing you a fantastic day and a year ahead filled with happiness and success. Thanks for being an awesome boss and making work so much fun! 🎉" },
 ];
 
